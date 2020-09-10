d117823c1e628ea90eb73f1c3ff62233066817e0
Alo <dosick7397@gmail.com>
2020-09-10 22:51:10 +0900

    change

--- Loading.js
+++ Loading.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function Loading(){
     return (
         <View style={styles.container}>
-            <Text style={styles.text}>Alo2</Text>
+            <Text style={styles.text}>alo12</Text>
         </View>
     );
 }
