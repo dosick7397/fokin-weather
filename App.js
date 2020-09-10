94ca6e7c860582c58c850cd3784df44e946e4951
Alo <dosick7397@gmail.com>
2020-09-10 16:06:35 +0900

    first commit

---
+++ App.js
@@ -0,0 +1,11 @@
+// App.js
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Loading from './Loading';
+
+
+export default function App() {
+  return (
+    <Loading />
+  );
+}
